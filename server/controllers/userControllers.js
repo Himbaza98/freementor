9c8d9b4f45fc170f57a7f824158210fe41cb03be
Chretien <bookchretien@gmail.com>
2019-09-01 20:10:22 +0200

    chore(signupAPI):fixing my signup backend
    hash password
    generating token
    
    [Finishes#168195494]

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,10 +1,16 @@
 import users from '../data/users';
 import schema from '../validators/validations';
 import Joi from 'joi';
-class User {
-    static SignUp(req, res) { //check sign up details if valid with joi
-        const { email, firstname, lastname, password, address, bio, expertise, occupation } = req.body;
+import { hashPassword } from '../helpers/bcryptPwd';
+import { checkThepassword } from '../helpers/bcryptPwd';
+import { getToken } from '../helpers/tokens';
 
+
+
+class User {
+
+    static SignUp(req, res) { //check sign up details if valid with joi
+        const { email, firstname, lastname, password, address, bio, expertise, occupation, } = req.body;
         let result = Joi.validate({ email, firstname, lastname, password }, schema.user_sign_up);
         if (result.error) {
             return res.status(400).json({ status: 400, message: `${result.error.details[0].message}` });
@@ -13,22 +19,47 @@ class User {
         const emailfound = users.find(user => user.email == email)
         if (!emailfound) {
 
-            users.push({ email, firstname, lastname, password, address, bio, expertise, occupation })
-            return res.status(200).send({
+            let id = users.length + 1;
+            let payload = { id, firstname, is_admin: false };
+
+            //hash the password and generate the token
+            const encryptedPassword = hashPassword(password);
+
+            let token = getToken(payload);
+            users.push({
+                id,
+                email,
+                firstname,
+                lastname,
+                password: encryptedPassword,
+                address,
+                bio,
+                expertise,
+                occupation,
+                is_admin: false,
+                role: 'mentee'
+            })
+
+
+            let newUser = { id, email, firstname, lastname, token };
+            return res.status(201).send({
                 status: 201,
                 message: "user created successfully",
-                data: {
-                    token: "string",
-                    message: "user created successfully",
-                    user: { email, firstname, lastname, address, bio, expertise, occupation },
-                }
+                data: newUser,
             })
+
+
         }
-        return res.status(409).send({
+        return res.status(401).send({
+            status: 401,
             error: "The user with email arleady exists"
-        })
+        });
+
+
+
     };
-
-
+  
 }
+
+
 export default User;
---
+++ server/helpers/tokens.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+export const getToken = (payload) => { return jwt.sign(payload, process.env.SECRET_KEY) };
+export const decoded = (req, res) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        return jwt.verify(token, process.env.SECRET_KEY);
+    } catch (error) {
+        return ({ status: 401, message: "There is no user with this token" });
+    }
+}
+
+export default { getToken, decoded };
